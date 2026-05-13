4c00b0a18e580670f2a80af6a874ef1d4dac6f0d
Saqib Ashraf Bhat <saqibashraf7402@gmail.com>
2026-05-13 12:49:46 +0530

    fixed filename error

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -2,7 +2,7 @@ const express = require("express")
 
 const router = express.Router()
 
-const Message = require("../models/Message")
+const Message = require("../models/message")
 
 router.post("/", async (req, res) => {
 
